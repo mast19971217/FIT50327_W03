ddcaecfb84bee3e97de1f7e2faac63047833bb85
zjw <zjw@DESKTOP-ZJW>
2021-07-13 22:24:19 +0800

    Add project files.

---
+++ FIT50327_MyModalFirst/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FIT50327_MyModalFirst
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
